59e967857de1e85f8929c879ccbf29186240d1bd
Jason Whelehon <wheelie33@gmail.com>
2018-03-02 11:31:46 -0600

    reformatting code

--- ContactManager/SignupForm.cs
+++ ContactManager/SignupForm.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ContactManager
+﻿namespace ContactManager
 {
     public class SignupForm
     {
         public bool Signup(string firstName, string lastName, string emailAddress)
         {
-
             var dataAccess = new DataAccess();
 
             var saved = dataAccess.SaveRecord(firstName, lastName, emailAddress);
